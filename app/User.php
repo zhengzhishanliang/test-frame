13cdb776509232cb0cb339eb816836725c2c6c01
67653 <anyuan@qq.com>
2021-07-16 22:50:00 +0800

    test1 first commit

--- app/User.php
+++ app/User.php
@@ -11,12 +11,17 @@ class User
         echo "hello php";
     }
 
+    public function test1()
+    {
+        echo 'test1';
+    }
+
     public function test2()
     {
         echo 'test2';
     }
 
-    public function test1()
+    public function test3()
     {
         echo 'test1';
         echo 'test3';
--- core/test1.php
+++ core/test1.php
@@ -2,3 +2,4 @@
 echo 'test1';
 echo 'test1.....2';
 echo 'test1.....3';
+echo 'test1';
